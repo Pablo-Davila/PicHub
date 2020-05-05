a7e5e85af83b3aabc6c93308ddb5933f3bf79e57
Pablo-Davila <59798058+Pablo-Davila@users.noreply.github.com>
2020-05-05 21:09:05 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,27 @@
-let date = new Date();
-
-let day = date.getDate();
-let month = date.getMonth() + 1;  // Months are 0-based for some reason...
-let year = date.getFullYear();
-
-let hour = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
-
-// Add a leading 0 if they're only one digit
-hour = hour.toString().padStart(2, "0");
-minutes = minutes.toString().padStart(2, "0");
-seconds = seconds.toString().padStart(2, "0");
-
-/* 
-   Print the current date
-   and a message to report that we're finished
-*/
-
-console.log("Loading finished.");
-console.log(`The current time is: ${day}/${month}/${year} ${hour}:${minutes}:${seconds}`);
+"use strict";
 
 $(main);
+
 function main() {
 
-    // FLECHAS
-    if(suficienteancho) {
-	$("#flechas").style.display = "flex";
-    }
-    else{
-	$("#flechas").style.display = "none";
-    }
+    let date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;  // Months are 0-based for some reason...
+    let year = date.getFullYear();
+
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    // Add a leading 0 if they're only one digit
+    hour = hour.toString().padStart(2, "0");
+    minutes = minutes.toString().padStart(2, "0");
+    seconds = seconds.toString().padStart(2, "0");
+
+    // Print the current date and a message to report that we're finished
+
+    console.log("Loading finished.");
+    console.log(`The current time is: ${day}/${month}/${year} ${hour}:${minutes}:${seconds}`);
 }
 
