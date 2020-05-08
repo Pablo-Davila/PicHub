e7946447044a4b6e8858ce41a858e93ce18973c7
Pablo-Davila <59798058+Pablo-Davila@users.noreply.github.com>
2020-05-08 20:58:24 +0200

    Minor improvements

--- imports.php
+++ imports.php
@@ -1,4 +1,5 @@
 
+<!-- Básico -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1,
 			       shrink-to-fit=no">
@@ -10,15 +11,15 @@
       crossorigin="anonymous">
 <link rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <link rel="stylesheet" href="css/style.css">
 
 <!-- Scripts importados -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
 	crossorigin="anonymous"></script> <!--JQuery-->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+<!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
 	crossorigin="anonymous"></script> <!--Pop-outs, tooltips...-->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
 	crossorigin="anonymous"></script> <!--Bootstrap-->
 
-<!-- Mis scripts -->
-<script src="js/scripts.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
