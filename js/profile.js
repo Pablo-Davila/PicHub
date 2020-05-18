00e4553497ec204e3ec672d8aecbeef0147365af
Pablo-Davila <59798058+Pablo-Davila@users.noreply.github.com>
2020-05-19 01:43:08 +0200

    Profile: retreive data frombackend

---
+++ js/profile.js
@@ -0,0 +1,210 @@
+"use strict";
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get('id');
+
+$(main);
+
+function main() {
+
+    // Mostrar botón de seguir
+    if(id != getUserId()) {
+	let btn_html = `
+	      <div id="follow"
+		 class="btn btn-info text-white ml-3 mb-2 d-inline-block">
+		Seguir
+	      </div>`;
+	$("#data-title-div").append($.parseHTML(btn_html));
+	$("#follow").click(toggleFollow);
+    }
+
+    // Actualizar seguir/dejar de seguir
+    updateFollow();
+
+    // Rellenar datos de usuario
+    console.log("Cargando datos de usuario...");
+    $.ajax({
+	method: "GET",
+	url: `http://localhost:3000/users/${id}`,
+	success: function(data) {
+	    $("#data-title").text("Perfil de " + data.user);
+	    $("#img-title").text("Imágenes de " + data.user);
+	    $("#name").text(data.name);
+	    $("#surnames").text(data.surname);
+	    $("#email").text(data.email);
+	    console.log("Datos de usuario cargados con éxito");
+	},
+	error: function(error) {
+            console.log("Error al acceder a los datos del usuario.");
+	    $("#errors-container").empty();
+	    $("#errors-container").append(getError("Error al acceder a los datos del usuario."));
+	}
+    });
+
+    // Cargar imágenes
+    console.log(`Cargando imágenes del usuario ${id}...`);
+    $.ajax({
+	method: "GET", url:
+	`http://localhost:3000/images?userId=${id}`,
+	success: displayPhotos,
+	error: function(error) {
+            console.log("Error al acceder a las imágenes del usuario.");
+	    $("#errors-container").empty();
+	    $("#errors-container").append(getError("Error al acceder a las imágenes del usuario."));
+	}
+    });
+}
+
+function updateFollow() {
+    $.ajax({
+	method: "GET",
+	url: `http://localhost:3000/follows?followerId=${getUserId()}`,
+	success: function(data) {
+	    let followed = false;
+	    let f_id;
+	    for(let f of data) {
+		if(f.targetId == id) {
+		    followed = true;
+		    f_id = f.id;
+		}
+	    }
+	    if(followed) $("#follow").text("Dejar de seguir");
+	    else $("#follow").text("Seguir");
+	},
+	error: function(error) {
+            console.log("Error al comprobar si se sigue al usuario.");
+	    $("#errors-container").empty();
+	    $("#errors-container").append(getError("Error al comprobar si se sigue al usuario."));
+	}
+    });
+}
+
+function postFollow() {
+    let data = {
+	"followerId": getUserId(),
+	"targetId": id
+    };
+    
+    $.ajax({
+	method: "POST",
+	url: "http://localhost:3000/follows",
+	data: JSON.stringify(data),
+	dataType: "json",
+	contentType: "application/json; charset=UTF-8",
+	processData: false,
+	success: function() {
+	    $("#follow").text("Dejar de seguir");
+	    console.log(`Se ha empezado a seguir al usuario con id ${id}.`);
+	},
+	error: function(error) {
+            console.log(`Error al seguir al usuario con id ${id}.`);
+	    $("#errors-container").empty();
+	    $("#errors-container").append(getError("Error al seguir al usuario."));
+	}
+    });
+}
+
+function deleteFollow(f_id) {
+    $.ajax({
+	type: "DELETE",
+	url: `http://localhost:3000/follows/${f_id}`,
+	headers: {
+	    "Authorization": "Bearer " + getToken()
+	},
+	success: function() {
+	    $("#follow").text("Seguir");
+	    console.log(`Se ha dejado de seguir al usuario ${id}.`);
+	},
+	error: function(error) {
+            console.log(`Error al dejar de seguir al usuario ${id}.`);
+	    $("#errors-container").empty();
+	    $("#errors-container").append(getError("Error al dejar de seguir al usuario."));
+	}
+    });
+}
+
+function toggleFollow() {
+    if(id == getUserId()) return;
+    $.ajax({
+	method: "GET",
+	url: `http://localhost:3000/follows?followerId=${getUserId()}`,
+	success: function(data) {
+	    let followed = false;
+	    let f_id;
+	    for(let f of data) {
+		if(f.targetId == id) {
+		    followed = true;
+		    f_id = f.id;
+		}
+	    }
+	    if(followed) deleteFollow(f_id);
+	    else postFollow();
+	},
+	error: function(error) {
+            console.log("Error al comprobar si se sigue al usuario.");
+	    $("#errors-container").empty();
+	    $("#errors-container").append(getError("Error al comprobar si se sigue al usuario."));
+	}
+    });
+}
+
+function displayPhotos(data) {
+    let row = $("div.container > div.row").last();
+    
+    let count = 0;
+    for (let photo of data) {
+	if(photo.private && id!=getUserId()) continue;
+	count++;
+
+	// HTML de la tarjeta
+	let card_str = `
+    	<div class="col-md text-center max-w-50">
+          <div class="card border-dark mb-4">
+            <a href="image_detail.php?id=${photo.id}">
+	      <div class="embed-responsive embed-responsive-4by3">
+		<img class="card-img-top embed-responsive-item"
+		     src=${photo.url}>
+	      </div>
+            </a>
+
+            <div class="card-body bg-dark">
+              <h5 class="card-title mb-1">${photo.title}</h5>
+              <hr class="mt-0">
+              <p class="card-text">
+		Etiquetas:
+              </p>
+            </div>
+          </div>
+	</div>`;
+
+	let card_html = $.parseHTML(card_str);
+	row.append(card_html);
+	updateAuthorName(photo.userId);
+	    
+	// Etiquetas
+	let tagList = $("p.card-text").last();
+	if(photo.tags != undefined) {
+	    for(let tag of photo.tags) {
+		let tag_html = $(
+		    "<span></span>",
+		    {
+			"class": "badge badge-primary",
+			text: tag,
+		    });
+		tagList.append(tag_html);
+	    }
+	    $("span.badge").after(" "); // Separar etiquetas
+	}
+	else tagList.text("Sin etiquetas");
+
+	if(count%2 == 0) {
+	    let new_row = $("<div></div>", {"class": "row w-99"});
+	    $("div.container").append(new_row);
+	    row = new_row;
+	}
+    }
+    console.log(`Mostradas ${count} imágenes`);
+}
+
+
--- profile.php
+++ profile.php
@@ -4,10 +4,16 @@
   <head>
     <?php include 'imports.php' ?>
     <title>PicHub - Perfil de usuario</title>
+    <script type="text/javascript" src="js/profile.js"></script>
   </head>
 
   <body>
     <?php include 'header.php' ?>
+
+    <!-- Errores -->
+    <div class="row w-60 mx-auto">
+      <div class="col-md" id="errors-container"></div>
+    </div>
     
     <div class="container txt-light">
       <div class="row">
@@ -16,13 +22,9 @@
 	<div class="col-md-5">
 	  
 	  <div class="row">
-	    <div class="col-md">
+	    <div id="data-title-div" class="col-md">
 	      </br></br>
-              <h3 class="ml-4 d-inline">Perfil de UserName1</h3>
-	      <a href="#followed"
-		 class="btn btn-info text-white ml-3 mb-2 d-inline-block">
-		Seguir
-	      </a>
+              <h3 id="data-title" class="ml-4 d-inline">Perfil de</h3>
 	    </div>
 	  </div>
 
@@ -33,19 +35,19 @@
 	    <div class="row p">
 	      <div class="col-md">
 		<h5>Nombre</h5>
-		<p>UserName1</p>
+		<p id="name"></p>
 	      </div>
 	    </div>
 	    <div class="row">
 	      <div class="col-md">
 		<h5>Apellidos</h5>
-		<p>UserSurname1</p>
+		<p id="surnames"></p>
 	      </div>
 	    </div>
 	    <div class="row">
 	      <div class="col-md">
 		<h5>Email</h5>
-		<p>Email1</p>
+		<p id="email"></p>
 	      </div>
 	    </div>
 
@@ -82,87 +84,15 @@
 	  <div class="row">
 	    <div class="col-md">
               </br></br>
-              <h3 class="ml-4">Imágenes de UserName1</h3>
+              <h3 id="img-title" class="ml-4 d-inline">Imágenes de</h3>
 	    </div>
 	  </div>
 
 	  <hr>
 
-	  <!-- Tarjetas sin autor, título mb-1 y línea mt-0, tarjeta mb-4 -->
-	  <!-- Fila 1 -->
-	  <div class="row">
-	    <div class="col-md text-center">
-              <div class="card border-dark mb-4">
-		<a href="image_detail.php">
-		  <img src="http://via.placeholder.com/300.png" class="card-img-top">
-		</a>
-
-		<div class="card-body bg-dark txt-light">
-		  <h5 class="mb-1">Título de la imagen 1</h5>
-		  <hr class="mt-0">
-		  <p class="card-text">
-		    Etiquetas:
-		    <span class="badge badge-primary">Paisaje</span>
-		    <span class="badge badge-primary">Naturaleza</span>
-		  </p>
-		</div>
-              </div>
-	    </div>
-
-	    <div class="col-md text-center">
-              <div class="card border-dark mb-4">
-		<a href="image_detail.php">
-		  <img src="http://via.placeholder.com/300.png" class="card-img-top">
-		</a>
-
-		<div class="card-body bg-dark txt-light">
-		  <h5 class="mb-1">Título de la imagen 2</h5>
-		  <hr class="mt-0">
-		  <p class="card-text">
-		    Etiquetas:
-		    <span class="badge badge-primary">Animales</span>
-		  </p>
-		</div>
-              </div>
-	    </div>
-	  </div>
-
-	  <!-- Fila 2 -->
-	  <div class="row">
-	    <div class="col-md text-center">
-              <div class="card border-dark mb-4">
-		<a href="image_detail.php">
-		  <img src="http://via.placeholder.com/300.png" class="card-img-top">
-		</a>
-
-		<div class="card-body bg-dark txt-light">
-		  <h5 class="mb-1">Título de la imagen 3</h5>
-		  <hr class="mt-0">
-		  <p class="card-text">
-		    Etiquetas:
-		    <span class="badge badge-primary">Paisaje</span>
-		    <span class="badge badge-primary">Naturaleza</span>
-		  </p>
-		</div>
-              </div>
-	    </div>
-
-	    <div class="col-md text-center">
-              <div class="card border-dark mb-4">
-		<a href="image_detail.php">
-		  <img src="http://via.placeholder.com/300.png" class="card-img-top">
-		</a>
-
-		<div class="card-body bg-dark txt-light">
-		  <h5 class="mb-1">Título de la imagen 4</h5>
-		  <hr class="mt-0">
-		  <p class="card-text">
-		    Etiquetas:
-		    <span class="badge badge-primary">Animales</span>
-		  </p>
-		</div>
-              </div>
-	    </div>
+	  <!-- Filas de imágenes -->
+	  <div class="container">
+	    <div class="row w-99"></div>
 	  </div>
 	  
 	</div>
