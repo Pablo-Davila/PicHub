245732215ba97380c7c5eadee814f7302778fe49
Pablo-Davila <59798058+Pablo-Davila@users.noreply.github.com>
2020-05-26 03:28:54 +0200

    Change iamge score system

--- js/trending_images.js
+++ js/trending_images.js
@@ -14,19 +14,49 @@ function main() {
 function displayPhotos(data) {
     let row = $("div.container > div.row").last();
 
-    data.sort(function(a, b){
-	return score(b) - score(a);
-    });
+    // Acumular promesas de cálculo de puntuaciones
+    let promises = [];
+    for(let i=0; i<data.length; i++) {
+	promises.push(
+	    new Promise( (resolve,reject) => {
+		$.ajax({
+		    method: "GET",
+		    url: `http://localhost:3000/votes?imageId=${data[i].id}`,
+		    success: function(votes) {
+			let numerador = 0;
+			let denominador = 0;
+			let aWeekAgo = new Date();
+			aWeekAgo.setDate(aWeekAgo.getDate() - 7);
+			for(let v of votes) {
+			    if(new Date(v.date) > aWeekAgo) {
+				denominador++;
+				if(v.like) numerador++;
+				else numerador--;
+			    }
+			}
+			let score = (denominador==0)? 0 : (numerador/denominador).toFixed(2);
+			resolve(new Array(data[i].id, score));
+		    },
+		    error: reject
+		});
+	    })
+	);
+    }
 
-    let autores = new Set();
-    let etiquetas = new Set();
-    let count = 0;
-    for (let photo of data) {
-	if(count == 10) break;
-	count++;
+    // Una vez estén todas actualizadas
+    Promise.all(promises).then( function(scores) {
+	let map = new Map(scores);
+	data.sort( (a,b) => map.get(b.id) - map.get(a.id));
 
-	// HTML de la tarjeta
-	let card_str = `
+	let autores = new Set();
+	let etiquetas = new Set();
+	let count = 0;
+	for(let photo of data) {
+	    if(count == 10) break;
+	    count++;
+
+	    // HTML de la tarjeta
+	    let card_str = `
     	<div class="col-md text-center max-w-50">
           <div class="card border-dark mb-4">
             <a href="image_detail.php?id=${photo.id}">
@@ -40,7 +70,7 @@ function displayPhotos(data) {
               <h5 class="card-title">${photo.title}</h5>
               <a name="auth-${photo.userId}" href="profile.php?id=${photo.userId}" class="card-text"></a>
               <hr>
-	      <p class="card-text">Puntuación: ${score(photo)}</p>
+	      <p class="card-text">Puntuación: ${map.get(photo.id)}</p>
               <p class="card-text">
 		Etiquetas:
               </p>
@@ -48,33 +78,40 @@ function displayPhotos(data) {
           </div>
 	</div>`;
 
-	let card_html = $.parseHTML(card_str);
-	row.append(card_html);
-	updateAuthorName(photo.userId);
-	
-	// Etiquetas
-	let tagList = $("p.card-text").last();
-	if(photo.tags != undefined) {
-	    for(let tag of photo.tags) {
-		let tag_src = `<span name="tag-${tag}" class="badge badge-primary"></span>`;
-		tagList.append($.parseHTML(tag_src));
-		etiquetas.add(tag);
+	    let card_html = $.parseHTML(card_str);
+	    row.append(card_html);
+	    updateAuthorName(photo.userId);
+	    
+	    // Etiquetas
+	    let tagList = $("p.card-text").last();
+	    if(photo.tags != undefined) {
+		for(let tag of photo.tags) {
+		    let tag_src = `<span name="tag-${tag}" class="badge badge-primary"></span>`;
+		    tagList.append($.parseHTML(tag_src));
+		    etiquetas.add(tag);
+		}
+		$("span.badge").after(" "); // Separar etiquetas
 	    }
-	    $("span.badge").after(" "); // Separar etiquetas
-	}
-	else tagList.text("Sin etiquetas");
+	    else tagList.text("Sin etiquetas");
 
-	if(count%2 == 0) {
-	    let new_row = $("<div></div>", {"class": "row w-99"});
-	    $("div.container").append(new_row);
-	    row = new_row;
+	    if(count%2 == 0) {
+		let new_row = $("<div></div>", {"class": "row w-99"});
+		$("div.container").append(new_row);
+		row = new_row;
+	    }
+	    
+	    // Actualizar nombres de autores y etiquetas
+	    for(let a of autores) updateAuthorName(a);
+	    for(let t of etiquetas) updateTagName(t);
 	}
 	
-	// Actualizar nombres de autores y etiquetas
-	for(let a of autores) updateAuthorName(a);
-	for(let t of etiquetas) updateTagName(t);
-    }
-    console.log(`Mostradas ${count} imágenes`);
+	console.log(`Mostradas ${count} imágenes`);
+    }).catch( function(error) {
+	console.log(`Error al acceder a los votos del sistema`, error);
+	let errores = $("#errors-container");
+	errores.empty();
+	errores.append(getError(`No se pudo acceder a los votos del sistema.`));
+    });
 }
 
 
