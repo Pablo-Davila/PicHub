4078a4f9c5d060d36dca35081f70e46725b9043b
Pablo-Davila <59798058+Pablo-Davila@users.noreply.github.com>
2020-05-26 04:02:48 +0200

    Link to to-do.html

--- header.php
+++ header.php
@@ -1,74 +1,43 @@
 
-<div id="theHeader"><!-- class="title-block"> -->
-  <!-- <h1 id="title">Galería fotográfica <i class="fa
-					     fa-file-image-o"
-					     aria-hidden="true"></i></h1>
-  <h3 id="subtitle">Creada por Pablo Dávila Herrero</h3> --> 
-
+<div id="theHeader">
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+
+    <!-- Logo -->
     <a href="index.php">
       <img id="logo" src="img/logo.png">
     </a>
-    <!--<a class="navbar-brand" href="#">Índice de navegación</a>-->
+
+    <!-- Botón para desplegar la navbar -->
     <button class="navbar-toggler" type="button"
 	    data-toggle="collapse" data-target="#navbarNav"
 	    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon bg-sdark txt-light rounded"></span>
     </button>
-    
+
     <div class="collapse navbar-collapse txt_light" id="navbarNav">
+      
       <!--Lado izquierdo-->
       <ul id="nav-izq" class="navbar-nav">
         <li class="nav-item">
           <a id="a-nuevo" class="nav-link" href="index.php">Nuevo</a>
         </li>
-        <!--<li class="nav-item dropdown">
-          <a id="a-trending" class="nav-link dropdown-toggle" href="#"
-	     id="navbarDesplegableId" data-toggle="dropdown"
-	     aria-haspopup="true" aria-expanded="false">
-	    Trending
-          </a>
-          <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
-	    <a class="dropdown-item" href="trending_users.php">Usuarios trending</a>
-	    <a class="dropdown-item" href="trending_images.php">Imágenes trending</a>
-          </div>
-        </li>
-        <li class="nav-item">
-	<li class="nav-item">
-          <a id="a-siguiendo" class="nav-link" href="following.php">Siguiendo</a>
-        </li>
-        <li class="nav-item">
-	<li class="nav-item">
-          <a id="a-etiquetas" class="nav-link" href="tags.php">Etiquetas</a>
-        </li>-->
-      </ul>
-
-      <!-- Centro -->
-      <!--<ul class="navbar-nav mx-auto">
-        <li class="nav-item">
-          <a id="a-login" class="nav-link" href="login.php">Login</a>
-        </li>
-        <li class="nav-item">
-          <a id="a-register" class="nav-link" href="register.php">Registro</a>
-        </li>        <li class="nav-item">
-          <a id="logout" class="nav-link" href="index.php">Cerrar sesión</a>
-        </li>
-      </ul>-->
-
-      <!-- Lado derecho -->
-      <ul id="nav-der" class="navbar-nav ml-auto">
-        <!--<li class="nav-item">
-          <a id="my-profile" class="nav-link" href="">Mi perfil</a>
-        </li>-->
       </ul>
       
+      <!-- Lado derecho -->
+      <ul id="nav-der" class="navbar-nav ml-auto"></ul>      
     </div>
+    
   </nav>
 </div>
 
+<!-- Botón lista to-do -->
+<a href="to-do.html" class="display-4" target="_blank">
+  <i id="to-do" class="fa fa-list"></i>
+</a>
+
 <!-- Botón nueva imagen -->
 <a href="image_edit.php">
-  <img id="plus" src="img/plus.svg" href="edit.php">
+  <img id="plus" src="img/plus.svg">
 </a>
 
 <hr>
