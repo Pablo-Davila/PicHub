f9ca33789a38b980052f688b526fbff5f7661446
Pablo-Davila <59798058+Pablo-Davila@users.noreply.github.com>
2020-05-26 04:03:10 +0200

    Link to GitHub Enterprise

--- footer.php
+++ footer.php
@@ -2,7 +2,13 @@
 <br/><br/>
 
 <hr>
-<p id="foot-text" class="footer txt-dark text-center">PicHub&copy - </p>
+<p id="foot-text" class="footer txt-dark text-center">
+  <a href="https://github.eii.us.es/pabdavher/PicHub"
+     class="h3" target="_blank">
+    <i class="fa fa-github txt-dark"></i>
+  </a>
+  PicHub&copy -
+</p>
 <p class="text-center txt-dark">
   Página diseñada por Pablo Dávila Herrero
 </p>
