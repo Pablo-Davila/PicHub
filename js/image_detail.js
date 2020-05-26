1b030bf0139d6d0a3c58bf5fa957301cf9af059e
Pablo-Davila <59798058+Pablo-Davila@users.noreply.github.com>
2020-05-26 03:21:28 +0200

    Allow voting

--- js/image_detail.js
+++ js/image_detail.js
@@ -3,7 +3,7 @@
 // Información a partir de la URL
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get('id');
+const id = parseInt(urlParams.get('id'));
 
 $(main);
 
@@ -13,8 +13,43 @@ function main() {
     loadSinglePhoto(id, false);
     
     if(getToken() != null) {
+
+	// Habilitar like/dislike
+	let score_str = `
+          <i id="dislike" class='fa fa-thumbs-down txt-dark h4' aria-hidden='true'></i>
+          <i id="like" class='fa fa-thumbs-up txt-dark h4' aria-hidden='true'></i>`;
+	$("#score-section").append($.parseHTML(score_str));
+	$("#like").click(toggleLike);
+	$("#dislike").click(toggleDislike);
+
+	// Actualizar color like-dislike
+	$.ajax({
+	    method: "GET",
+	    url: `http://localhost:3000/votes?imageId=${id}&userId=${getUserId()}`,
+	    success: function(data) {
+		if(data.length != 0) {
+		    if(data[0].like) {
+			$("#like").toggleClass("txt-dark");
+			$("#like").toggleClass("text-success");
+		    }
+		    else {
+			$("#dislike").toggleClass("txt-dark");
+			$("#dislike").toggleClass("text-danger");
+		    }
+		}
+	    },
+	    error: function(error) {
+		console.log("Error al acceder a los votos del sistema.");
+		$("#errors-container").empty();
+		$("#errors-container").append(getError("No se pudo acceder a los votos del sistema."));
+	    }
+	});
+
+	// Actualizar puntuación
+	$("#score").attr("name", `score-${id}`);
+	updateScore(id);
 	
-	// Allow new comments
+	// Permitir nuevos comentarios
 	$("#newComment").attr("disabled", false);
 	$("#newComment").attr("placeholder", "Escribe un nuevo comentario.");
 	let btn_str = `
@@ -30,6 +65,7 @@ function main() {
     }
 }
 
+// Comentarios
 function newComment() {
     if($("#newComment").val() == "") return;
     
@@ -82,6 +118,140 @@ function loadComments() {
 	error: function(error) {
 	    console.log("Error al cargar los comentarios.");
 	    $("#errors-container").append(getError("No se pudo cargar los comentarios."));
+	}
+    });
+}
+
+// Votos
+function toggleLike() {
+    new Promise( (resolve,reject) => {
+	$.ajax({
+	    method: "GET",
+	    url: `http://localhost:3000/votes?imageId=${id}&userId=${getUserId()}`,
+	    success: resolve,
+	    error: reject
+	});
+    }).then( function(data) {
+	if(data.length == 0) newVote(true);
+	else if(data[0].like == true) {
+	    removeVote(data[0].id);
+	}
+	else {
+	    updateVote(data[0].id, true);
+	}
+    }).catch( function(error) {
+	console.log("Error al acceder a los votos del sistema.");
+	$("#errors-container").empty();
+	$("#errors-container").append(getError("No se pudo acceder a los votos del sistema."));
+    });
+
+    // Respuesta visual
+    $("#like").toggleClass("text-success");
+    $("#like").toggleClass("txt-dark");
+    $("#dislike").removeClass("text-danger");
+    $("#dislike").addClass("txt-dark");
+}
+
+function toggleDislike() {
+    new Promise( (resolve,reject) => {
+	$.ajax({
+	    method: "GET",
+	    url: `http://localhost:3000/votes?imageId=${id}&userId=${getUserId()}`,
+	    success: resolve,
+	    error: reject
+	});
+    }).then( function(data) {
+	if(data.length == 0) newVote(false);
+	else if(data[0].like == true) {
+	    updateVote(data[0].id, false);
+	}
+	else {
+	    removeVote(data[0].id);
+	}
+    }).catch( function(error) {
+	console.log("Error al acceder a los votos del sistema.");
+	$("#errors-container").empty();
+	$("#errors-container").append(getError("No se pudo acceder a los votos del sistema."));
+    });
+
+    // Respuesta visual
+    $("#dislike").toggleClass("text-danger");
+    $("#dislike").toggleClass("txt-dark");
+    $("#like").removeClass("text-success");
+    $("#like").addClass("txt-dark");
+}
+
+function newVote(like) {
+    let data = {
+	"like": like,
+	"date": (new Date()).toISOString(),
+	"userId": getUserId(),
+	"imageId": id
+    };
+    
+    $.ajax({
+	method: "POST",
+	url: "http://localhost:3000/votes",
+	data: JSON.stringify(data),
+	dataType: "json",
+	contentType: "application/json; charset=UTF-8",
+	processData: false,
+	headers: {
+	    "Authorization": "Bearer " + getToken()
+	},
+	success: function() {
+	    updateScore(id);
+	    console.log(`Nuevo voto:  ${(like)? "like" : "dislike"}`);
+	},
+	error: function(error) {
+	    console.log("Error al crear la imagen.");
+	    $("#errors-container").empty();
+	    $("#errors-container").append(getError("No se ha podido crear la imagen."));
+	}
+    });
+}
+
+function removeVote(voteId) {
+    $.ajax({
+	type: "DELETE",
+	url: `http://localhost:3000/votes/${voteId}`,
+	headers: {
+	    "Authorization": "Bearer " + getToken()
+	},
+	success: function() {
+	    updateScore(id);
+	    console.log("Voto eliminado");
+	},
+	error: function() {
+	    console.log("Error al eliminar el voto.");
+	    $("#errors-container").empty();
+	    $("#errors-container").append(getError("No se pudo eliminar el voto."));
+	}
+    });
+}
+
+function updateVote(voteId, like) {
+    let data = {
+	"like": like,
+	"date": (new Date()).toISOString()
+    };
+    $.ajax({
+	type: "PATCH",
+	url: "http://localhost:3000/votes/" + voteId,
+	data: JSON.stringify(data),
+	contentType: "application/json; charset=UTF-8",
+	processData: false,
+	headers: {
+	    "Authorization": "Bearer " + getToken()
+	},
+	success: function() {
+	    updateScore(id);
+	    console.log("Voto cambiado a " + ((like)? "like" : "dislike"));
+	},
+	error: function(error) {
+	    console.log("Error al actualizar el voto.");
+	    $("#errors-container").empty();
+	    $("#errors-container").append(getError("No se ha podido actualizar el voto."));
 	}
     });
 }
