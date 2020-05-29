3c36b8a0e660afdb007981ad6b59c161a45b86c6
Pablo-Davila <59798058+Pablo-Davila@users.noreply.github.com>
2020-05-29 21:33:25 +0200

    Minor improvement

--- js/footer.js
+++ js/footer.js
@@ -4,4 +4,4 @@ let f = new Date();//.getFullYear();
 let footer = document.getElementById("foot-text");
 let fmonth = (f.getMonth()+1).toString().padStart(2, "0");
 let fday = f.getDate().toString().padStart(2, "0");
-footer.innerHTML += f.getFullYear() + "/" + fmonth + "/" + fday;
+footer.innerHTML += fday + "/" + fmonth + "/" + f.getFullYear();
